94c539cb7a508fd2d341c6d52a4f4acbf46ed416
therealmarvin <nakedtoothbrush@hotmail.com>
2023-02-01 19:52:07 -0500

    make training code more generic between reference and transformer

--- main_base.py
+++ main_base.py
@@ -1,62 +1,8 @@
-import os
-from os import path
-
-import numpy as np
-import torch
-import torch.nn as nn
-from sklearn.metrics import classification_report, confusion_matrix
-from torch import optim
 from torch.utils.tensorboard import SummaryWriter
 
+from common_training_setup import run_specific_experiment
 from helpers.dataset_helpers import get_mnist_sets
-from helpers.metrics_helpers import arg_max_accuracy
 from models.mlp import MLP
-from train_eval.eval import evaluate
-from train_eval.training import train
-
-
-def run_specific_experiment(summary, model, datasets, nb_epochs, batch_size, learning_rate):
-    train_set, test_set = datasets
-    model_name = model.__class__.__name__
-
-    summary.add_hparams({"model_name": model_name,
-                         "learning rate": learning_rate,
-                         "batch size": batch_size,
-                         "max epochs": nb_epochs}, {})
-
-    out_folder = "saved_models/{}".format(model_name)
-    if not path.exists(out_folder):
-        os.makedirs(out_folder)
-
-    save_file = "{}/best.model".format(out_folder)
-
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4)
-
-    if torch.cuda.is_available():
-        model = model.cuda()
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    train(model,
-          train_set,
-          optimizer,
-          criterion,
-          None,
-          batch_size,
-          nb_epochs,
-          True,
-          summary,
-          save_file,
-          early_stop=None,
-          true_index=1)
-    print('Finished Training')
-
-    metrics = {"loss": criterion, "acc": arg_max_accuracy}
-    y_pred, y_true, valid_loss = evaluate(model, test_loader, metrics)
-    y_pred = np.array(y_pred).argmax(1)
-    print(classification_report(y_true, y_pred))
-    print(confusion_matrix(y_true, y_pred))
 
 
 def main_mlp():
--- main_vit.py
+++ main_vit.py
@@ -1,70 +1,8 @@
-import torch
-import torchvision
-from sklearn.metrics import classification_report, confusion_matrix
 from torch.utils.tensorboard import SummaryWriter
-import torch.nn as nn
-from torch import optim
 
-import os
-from os import path
-
-import numpy as np
-
-from torchvision import transforms
-
+from common_training_setup import run_specific_experiment
 from helpers.dataset_helpers import get_mnist_sets
-from helpers.metrics_helpers import arg_max_accuracy
 from models.vit import ViT
-from train_eval.eval import evaluate
-from train_eval.training import train
-
-
-def run_specific_experiment(summary, model):
-    n_epochs = 5
-    batch_size = 128
-    learning_rate = 0.00005
-
-    model_name = "ViT"
-
-    summary.add_hparams({"learning rate": learning_rate,
-                         "batch size": batch_size,
-                         "max epochs": n_epochs}, {})
-
-    out_folder = "saved_models/{}".format(model_name)
-    if not path.exists(out_folder):
-        os.makedirs(out_folder)
-
-    save_file = "{}/best.model".format(out_folder)
-
-    train_set, test_set = get_mnist_sets()
-    # train_set, test_set = get_cifar10_sets()
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4)
-
-    if torch.cuda.is_available():
-        model = model.cuda()
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    train(model,
-          train_set,
-          optimizer,
-          criterion,
-          None,
-          batch_size,
-          n_epochs,
-          True,
-          summary,
-          save_file,
-          early_stop=None,
-          true_index=1)
-    print('Finished Training')
-
-    metrics = {"loss": criterion, "acc": arg_max_accuracy}
-    y_pred, y_true, valid_loss = evaluate(model, test_loader, metrics)
-    y_pred = np.array(y_pred).argmax(1)
-    print(classification_report(y_true, y_pred))
-    print(confusion_matrix(y_true, y_pred))
 
 
 def main_vit():
@@ -76,7 +14,12 @@ def main_vit():
                 group_channel=False,
                 nb_encoder_blocks=2,
                 nb_heads=2)
-    run_specific_experiment(summary, model)
+
+    nb_epochs = 5
+    batch_size = 128
+    learning_rate = 0.00005
+    datasets = get_mnist_sets()
+    run_specific_experiment(summary, model, datasets, nb_epochs, batch_size, learning_rate)
     summary.close()
 
 

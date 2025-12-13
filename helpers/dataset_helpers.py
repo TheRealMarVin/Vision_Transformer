5ddb0a9da7864f3e0becf6a19e53052563e9da72
therealmarvin <nakedtoothbrush@hotmail.com>
2025-12-13 06:58:15 -0500

    support stl10 dataset

--- helpers/dataset_helpers.py
+++ helpers/dataset_helpers.py
@@ -1,5 +1,6 @@
 import torchvision
 from torchvision import transforms
+from torchvision.datasets import STL10
 
 
 def get_mnist_sets():
@@ -30,5 +31,38 @@ def get_cifar10_sets():
 
     train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
     test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+
+    return train_set, test_set, image_resolution
+
+def get_stl10_sets():
+    image_resolution = (3, 96, 96)
+
+    transform_train = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(96, padding=4),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                             std=(0.5, 0.5, 0.5))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                             std=(0.5, 0.5, 0.5))
+    ])
+
+    train_dataset = STL10(
+        root="./data",
+        split="train",
+        download=True,
+        transform=transform_train
+    )
+
+    test_dataset = STL10(
+        root="./data",
+        split="test",
+        download=True,
+        transform=transform_test
+    )
 
     return train_set, test_set, image_resolution

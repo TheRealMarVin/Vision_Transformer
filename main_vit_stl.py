e39a1902a378212449c0e48c76536583bd8d435a
therealmarvin <nakedtoothbrush@hotmail.com>
2025-12-13 07:12:06 -0500

    start vit with linear on STL (rgb)... argggg not working

---
+++ main_vit_stl.py
@@ -0,0 +1,30 @@
+from torch.utils.tensorboard import SummaryWriter
+
+from common_training_setup import run_specific_experiment
+from helpers.dataset_helpers import get_stl10_sets
+from models.embeddings.linear_embedding import LinearEmbedding
+from models.vision_transformer_classifier import ViTClassifier
+
+
+def main_vit():
+    summary = SummaryWriter()
+    patch_size = (8,8)
+    embedding_size = 64
+    img_size = (3, 96, 96)
+
+    embedding_layer = LinearEmbedding(img_size=img_size,
+                                      patch_size=patch_size,
+                                      embedding_size=embedding_size)
+    model = ViTClassifier(embedding_layer=embedding_layer, img_size=img_size,
+                          nb_output=10, nb_encoder_blocks=6, nb_heads=4)
+
+    train_config_file = "config/training_params.ini"
+    train_set, test_set, image_size = get_stl10_sets()
+    run_specific_experiment(summary, model, (train_set, test_set), train_config_file)
+    summary.close()
+
+
+if __name__ == "__main__":
+    print("Hello")
+    main_vit()
+    print("Done")
